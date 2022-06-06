d33c1b5da4fc9f2cc60cc8c81fa4a410b320508e
George Bezerra <george.bezerra@fgv.br>
2022-06-06 17:22:07 -0300

    Criando o projeto

---
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,16 @@
+import { Header } from "./components/Header";
+import "./global.css";
+ 
+
+function App() {
+  return (
+    <>
+  <Header />
+  
+       
+      <h1>Ola mundo</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01-fundamentos-reactjs/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import style from './Header.module.css';
+
+export function Header(){
+    return(    
+        
+      <header  className={style.header} >
+          <strong  >Ignite feed</strong>
+      </header>  
+    );
+}
